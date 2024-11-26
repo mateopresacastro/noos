1c97c430e76cf06f400c41de16c2b2dca02702e3
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-11-26 12:39:12 +0100

    chore: Refactor

---
+++ components/upload/steps/five.tsx
@@ -0,0 +1,63 @@
+import Dropzone from "react-dropzone";
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { UploadFormSchema } from "@/components/upload-form-schema";
+import { UseFormReturn } from "react-hook-form";
+import type { Dispatch, SetStateAction } from "react";
+import { Input } from "@/components/ui/input";
+
+export default function StepFive({
+  form,
+  setDirection,
+  setStepIndex,
+}: {
+  form: UseFormReturn<UploadFormSchema>;
+  setDirection: Dispatch<SetStateAction<number>>;
+  setStepIndex: Dispatch<SetStateAction<number>>;
+}) {
+  return (
+    <FormField
+      control={form.control}
+      name="zipFile"
+      render={({ field }) => (
+        <FormItem>
+          <FormControl>
+            <Dropzone
+              onDrop={(acceptedFiles) => {
+                if (acceptedFiles.length <= 0) return;
+                field.onChange(acceptedFiles[0]);
+                setDirection(1);
+                setStepIndex(5);
+              }}
+              accept={{ "application/zip": [".zip"] }}
+              maxFiles={1}
+            >
+              {({ getRootProps, getInputProps, acceptedFiles }) => (
+                <section className="bg-neutral-900 p-6 rounded-xl border-neutral-600 border w-full border-dashed cursor-pointer active:bg-neutral-800/80 hover:bg-neutral-800 transition-all duration-150">
+                  <div
+                    {...getRootProps()}
+                    className="flex items-center justify-center h-48"
+                  >
+                    <Input {...getInputProps()} />
+                    <p className="text-neutral-500 text-xs text-center sm:text-left">
+                      {acceptedFiles.length > 0 || field.value
+                        ? `Selected file: ${
+                            acceptedFiles[0]?.name ?? field.value?.name
+                          }. Go to the next step to continue.`
+                        : "Drag and drop your ZIP file here, or click to select."}
+                    </p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
---
+++ components/upload/steps/one.tsx
@@ -0,0 +1,63 @@
+import Dropzone from "react-dropzone";
+import { FormField, FormItem, FormControl } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import type { UploadFormSchema } from "@/components/upload-form-schema";
+import type { Dispatch, SetStateAction } from "react";
+import type { UseFormReturn } from "react-hook-form";
+
+export default function StepOne({
+  form,
+  setStepIndex,
+  setDirection,
+}: {
+  form: UseFormReturn<UploadFormSchema>;
+  setStepIndex: Dispatch<SetStateAction<number>>;
+  setDirection: Dispatch<SetStateAction<number>>;
+}) {
+  return (
+    <FormField
+      control={form.control}
+      name="samples"
+      render={({ field }) => (
+        <FormItem>
+          <FormControl>
+            <Dropzone
+              onDrop={(acceptedFiles) => {
+                const samplesWithNames = acceptedFiles.map((file) => ({
+                  file,
+                  generatedName: file.name.replace(/\.[^/.]+$/, ""), // Remove extension
+                }));
+
+                field.onChange(samplesWithNames);
+                setStepIndex(1);
+                setDirection(1);
+              }}
+              accept={{ "audio/*": [".mp3"] }}
+            >
+              {({ getRootProps, getInputProps, acceptedFiles }) => (
+                <section className="bg-neutral-900 p-6 rounded-xl border-neutral-600 border w-full border-dashed cursor-pointer active:bg-neutral-800/80 hover:bg-neutral-800 transition-all duration-150">
+                  <div
+                    {...getRootProps()}
+                    className="flex items-center justify-center h-48"
+                  >
+                    <Input {...getInputProps()} />
+                    <p className="text-neutral-500 text-xs text-center sm:text-left">
+                      {acceptedFiles?.length > 0 || field.value?.length > 0
+                        ? `${
+                            field.value?.length > 0
+                              ? field.value?.length
+                              : acceptedFiles.length
+                          } files selected. Go to the next step to continue.`
+                        : "Drag and drop your samples here. Or click to select."}
+                    </p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+          </FormControl>
+        </FormItem>
+      )}
+    />
+  );
+}
