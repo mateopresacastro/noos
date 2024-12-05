0bdbd098c914d69c0f24537b34a3a6f85d71f4ce
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-12-05 15:38:10 +0100

    fix: #109

--- src/components/clerk.tsx
+++ src/components/clerk.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { resize } from "@/utils";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
 
 import {
   DropdownMenu,
@@ -24,7 +23,6 @@ import {
 
 export default function Clerk() {
   const { user } = useUser();
-  const router = useRouter();
   const clerk = useClerk();
   let userName: string;
   let imageUrl: string;
@@ -32,7 +30,6 @@ export default function Clerk() {
   if (user && user.username) {
     userName = user.username;
     imageUrl = resize(user.imageUrl ?? "");
-    router.prefetch(`/${userName}`);
   }
 
   return (
@@ -53,12 +50,16 @@ export default function Clerk() {
       </SignedOut>
       <SignedIn>
         <div className="w-fit flex items-center gap-3 lg:gap-4">
-          <Link href="/upload" prefetch={true}>
+          <Link href="/upload" prefetch={true} className="hidden sm:block">
             <Button size="sm" className="font-bold">
               Upload
             </Button>
           </Link>
-          <Link href="/dashboard/general" prefetch={true}>
+          <Link
+            href="/dashboard/general"
+            prefetch={true}
+            className="hidden sm:block"
+          >
             <Button size="sm" className="font-bold" variant="secondary">
               Dashboard
             </Button>
@@ -73,13 +74,26 @@ export default function Clerk() {
                 className="rounded-full object-cover size-7 hover:opacity-80 transition-opacity duration-150 active:opacity-60"
               />
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="dark:bg-neutral-900 mt-2 rounded-xl mr-2">
-              <DropdownMenuItem
-                className="text-sm rounded-lg cursor-pointer"
-                onClick={() => router.push(`/${userName}`)}
+            <DropdownMenuContent className="dark:bg-neutral-900 sm:mt-2 rounded-xl mr-2">
+              <Link href={`/${userName!}`} prefetch={true}>
+                <DropdownMenuItem className="text-sm rounded-lg cursor-pointer">
+                  Profile
+                </DropdownMenuItem>
+              </Link>
+              <Link
+                href="/dashboard/general"
+                prefetch={true}
+                className="block sm:hidden"
               >
-                Profile
-              </DropdownMenuItem>
+                <DropdownMenuItem className="text-sm rounded-lg cursor-pointer">
+                  Dashboard
+                </DropdownMenuItem>
+              </Link>
+              <Link href="/upload" prefetch={true} className="block sm:hidden">
+                <DropdownMenuItem className="text-sm rounded-lg cursor-pointer">
+                  Upload
+                </DropdownMenuItem>
+              </Link>
               <DropdownMenuItem
                 className="text-sm rounded-lg cursor-pointer"
                 onClick={() => clerk.signOut()}
