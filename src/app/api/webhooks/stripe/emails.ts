60c2fba5f66c189141b964b2d2f70ce7c9b245db
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-12-15 12:55:54 +0100

    feat: #71

---
+++ src/app/api/webhooks/stripe/emails.ts
@@ -0,0 +1,59 @@
+import log from "@/log";
+import sgMail from "@sendgrid/mail";
+
+export async function sendEmailToCustomer(
+  email: string,
+  name: string,
+  downloadUrl: string
+) {
+  if (!process.env.SENDGRID_API_KEY) {
+    await log.warn("SENDGRID_API_KEY not set, skipping email");
+    return;
+  }
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: "mateopresacastro@gmail.com",
+    subject: "Your sample pack is ready!",
+    text: `Hey ${name}, your sample pack is ready to download at ${downloadUrl}`,
+    html: `<p>Hey ${name}, your sample pack is ready to download at ${downloadUrl}</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    await log.error(
+      `Error sending email with link to ${email}, ${name}, download url:${downloadUrl}`,
+      error
+    );
+    throw error;
+  }
+}
+
+export async function notifySale(email: string) {
+  if (!process.env.SENDGRID_API_KEY) {
+    await log.warn("SENDGRID_API_KEY not set, skipping email");
+    return;
+  }
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: email,
+    from: "mateopresacastro@gmail.com",
+    subject: "Your sample pack is now available!",
+    text: `Hey ${name}, your just made a sale! Check it out on your dashboard.`,
+    html: `<p>Hey ${name}, your just made a sale! Check it out on your dashboard.</p>`,
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    await log.error(
+      `Error sending email to ${email}, with sale notification.`,
+      error
+    );
+    throw error;
+  }
+}
